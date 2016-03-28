5b490c9c92aef2e441254d17ce6ece287e047b37
sskyor <sskyor@163.com>
2016-03-28 18:51:27 +0800

    添加活动

--- api/app/Http/Controllers/ActivityController.php
+++ api/app/Http/Controllers/ActivityController.php
@@ -15,9 +15,28 @@ class ActivityController extends Controller
     public function getAdd(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required|min:2|max:255',
+            'start_at'=> 'required|date',
+            'end_at' => 'required|date',
+            'trigger_id'=>'required|exists:triggers.id',
+            'des'=>'required',
+            'enable'=> 'required|in:0,1'
         ]);
         if($validator->fails()){
             return $this->outputJson(10000,$validator->errors());
+        }
+        $activity = new Activity;
+        $activity->name = $request->name;
+        $activity->start_at = $request->start_at;
+        $activity->end_at = $request->end_at;
+        $activity->name = $request->name;
+        $activity->trigger_id = $request->trigger_id;
+        $activity->des = $request->des;
+        $activity->enable = $request->enable;
+        $res = $activity->save();
+        if($res){
+            return $this->outputJson(0,array('insert_id'=>$activity->id));
+        }else{
+            return $this->outputJson(10000,array('error_msg'=>'Insert Failed!'));
         }
     }
 
