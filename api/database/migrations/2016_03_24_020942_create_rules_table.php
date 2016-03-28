6064a04966a41c4a74e15e85ef14a40be1f7366e
sskyor <sskyor@163.com>
2016-03-28 16:52:27 +0800

    子规则表添加

--- api/database/migrations/2016_03_24_020942_create_rules_table.php
+++ api/database/migrations/2016_03_24_020942_create_rules_table.php
@@ -19,7 +19,58 @@ class CreateRulesTable extends Migration
             $table->integer('rule_id');
             $table->timestamps();
         });
-        //
+        //子规则表
+        //注册
+        Schema::create('rule_register', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('min_time');
+            $table->timestamp('max_time');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_invite', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('is_invite');
+            $table->tinyInteger('invites');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_userlevel', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('min_level');
+            $table->tinyInteger('max_level');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_usercredit', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('min_credit');
+            $table->tinyInteger('max_credit');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_balance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('min_balance');
+            $table->tinyInteger('max_balance');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_firstcast', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('min_firstcast');
+            $table->tinyInteger('max_firstcast');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_cast', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('min_cast');
+            $table->tinyInteger('max_cast');
+            $table->timestamps();
+        });
+
+
     }
 
     /**
