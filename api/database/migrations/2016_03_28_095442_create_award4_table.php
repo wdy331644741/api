091d874296f22b6a27129e54fa670f3f7a18dcb2
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-04-22 18:29:04 +0800

    奖品接口字段修改

--- api/database/migrations/2016_03_28_095442_create_award4_table.php
+++ api/database/migrations/2016_03_28_095442_create_award4_table.php
@@ -20,7 +20,6 @@ class CreateAward4Table extends Migration
             $table->tinyInteger('effective_time_type',false,true);//有效时间类型
             $table->string('effective_time_info',32);//有效时间信息
             $table->tinyInteger('platform_type',false,true);//平台端
-            $table->tinyInteger('activity_channel',false,true);//活动渠道
             $table->integer('created_at',false,true);//创建时间
             $table->integer('updated_at',false,true);//修改时间
         });
