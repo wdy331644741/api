75fce79b06bdeffdd651f072ef7c4840d0456149
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-04-27 13:40:26 +0800

    奖品管理修改字段

--- database/migrations/2016_03_28_095442_create_award3_table.php
+++ database/migrations/2016_03_28_095442_create_award3_table.php
@@ -14,14 +14,15 @@ class CreateAward3Table extends Migration
     {
         Schema::create('award_3', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',64);//名称
-            $table->integer('red_max_money',false,true);//红包金额
-            $table->integer('percentage',false,true);//红包金额
+            $table->string('name',64);//关联活动
+            $table->tinyInteger('experience_amount_type',false,true);//体验类型
+            $table->integer('experience_amount_money',false,true);//体验金额
+            $table->tinyInteger('experience_amount_multiple',false,true);//体验金额倍数
             $table->tinyInteger('effective_time_type',false,true);//有效时间类型
-            $table->string('effective_time_info',32);//有效时间信息
-            $table->integer('investment_threshold',false,true);//投资门槛
-            $table->tinyInteger('project_duration_type',false,true);//项目期限
-            $table->tinyInteger('project_type',false,true);//项目类型
+            $table->tinyInteger('effective_time_day',false,true);//有效时间顺延天数
+            $table->integer('effective_time_start',false,true);//有效时间开始时间
+            $table->integer('effective_time_end',false,true);//有效时间结束时间
+            $table->string('product_id',128);//产品ID
             $table->tinyInteger('platform_type',false,true);//平台端
             $table->string('limit_desc',32);//限制说明
             $table->integer('created_at',false,true);//创建时间
