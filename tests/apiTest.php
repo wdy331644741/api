a51df5cd8667b410159375f92f12a7fc1410bd86
zhaodongran <zhaodongran@wanglibank,com>
2016-04-28 18:44:07 +0800

    apiTEST 修改

--- tests/apiTest.php
+++ tests/apiTest.php
@@ -16,26 +16,26 @@ class apiTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testGetRuleList(){
-        $this->get('/rule/rulelist/3')
+    public function testRuleList(){
+        $this->get('/activity/rule-list/1')
             ->seeJsonStructure([
                 'error_code',
-                'data' =>[
-                    '*'=>['id','rule_type'],
+                'data'=> [
+                  '*'=>['id','activity_id','rule_type']
                 ],
             ]);
     }
 
-    public function testRuleAdd(){
-        $this->post('/rule/add/register',['activity_id'=>3,'max_time'=>'2016-03-03 15:16:17','min_time'=>'2016-02-03 15:16:17'])
+    public function testPostRuleAdd(){
+        $this->post('/activity/rule-add/register',['activity_id'=>2,'max_time'=>'2016-03-03 15:16:17','min_time'=>'2016-02-03 15:16:17'])
             ->seeJsonStructure([
                 'error_code',
                 'data' =>['insert_id'],
             ]);
     }
 
-    public function testRelease(){
-        $this->post('/activity/release',['id'=>3])
+    public function testPostRelease(){
+        $this->post('/activity/release',['id'=>2])
             ->seeJson([
                 'error_code'=>0,
             ]);
