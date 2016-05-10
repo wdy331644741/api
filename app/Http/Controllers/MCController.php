5e1dd9b11644bbbc62c2ff3094771e76f7b0ee57
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-05-10 18:57:55 +0800

    发送奖励接口修改

--- app/Http/Controllers/MCController.php
+++ app/Http/Controllers/MCController.php
@@ -11,18 +11,12 @@ use App\Jobs\SendReward;
  * Date: 2016/4/21
  * Time: 17:48
  */
-class SendRewardController extends Controller{
-    public function postTrigger(Request $request){
+class MCController extends Controller{
+    public function postCallBack(Request $request){
+        //触发的事件
+        $triggerName = isset($request->trigger) ? trim($request->trigger) : '';
         //触发的用户ID
         $userID = isset($request->user_id) ? intval($request->user_id) : 0;
-        if(empty($userID)){
-            return $this->outputJson(PARAMS_ERROR,array('error_msg'=>'用户id不能为空'));
-        }
-        //触发的事件
-        $triggerName = isset($request->trigger_name) ? trim($request->trigger_name) : '';
-        if(empty($triggerName)){
-            return $this->outputJson(PARAMS_ERROR,array('error_msg'=>'用户的触发事件不能为空'));
-        }
         //查询出该用户触发匹配的活动信息
         $where['name'] = $triggerName;
         $activityInfo = Activity::where($where)->get()->toArray();
