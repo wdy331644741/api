0ab5f46d61d3d245bc39796ec769b886eb8f1d09
zhaodongran <zhaodongran@wanglibank,com>
2016-06-23 11:59:35 +0800

    平台配置

--- config/cms.php
+++ config/cms.php
@@ -3,4 +3,13 @@ return [
     //图片http地址
     'img_http_url' => env('IMG_HTTP_URL'),
 
+    //平台配置
+    'platform'=>[
+        0=>'全平台',
+        1=>'IOS端',
+        2=>'Android端',
+        3=>'移动端',
+        4=>'PC端'
+    ]
+
 ];
