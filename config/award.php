90f6ebee44b27591590ca7d64df00828ddc444b9
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-07-15 11:45:43 +0800

    常量定义

--- config/award.php
+++ config/award.php
@@ -2,5 +2,14 @@
 return [
     //图片http地址
     'account_http_url' => env('ACCOUNT_HTTP_URL'),
-
+    //项目期限类型
+    'project_duration_type'=>[
+        1=>'不限',
+        2=>'月',
+        3=>'月及以上',
+        4=>'月及以下',
+        5=>'日',
+        6=>'日及以上',
+        7=>'日及以下'
+    ]
 ];
