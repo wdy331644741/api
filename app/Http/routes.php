cc7d210cf59a692a194a15b597d29226b3fd27a4
wangning <wangning@wanglibank.com>
2016-07-21 11:13:02 +0800

    域名增加子路径

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,29 +1,38 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::group(['prefix' => 'yunying'], function() {
+    Route::get('/', function () {
+        return 'server is ok';
+    });
 
 
+    Route::controller('activity', 'ActivityController');
+    Route::controller('account', 'AccountController');
+    Route::controller('channel', 'ChannelController');
+    Route::controller('cms/content','Cms\ContentController');
+    Route::controller('open','OpenController');
+    //奖品路由
+    Route::controller('award', 'AwardController');
+
+    //触发发奖路由
+    Route::controller('mc', 'MessageCenterController');
+
+    Route::controller('rpc', 'RpcController');
+
+    //图片管理
+    Route::controller('img', 'ImgManageController');
+    //图片地址转调
+    Route::get('/enclosures/{url}',function ($url) {
+        $img = file_get_contents(base_path()."/storage/images/{$url}");
+        return Response::make($img)->header('Content-Type', '');
+    });
+    //兑换码
+    Route::controller('redeem', 'RedeemController');   
+});
+    
 Route::get('/', function () {
     return 'server is ok';
 });
-
-/*
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-    Route::get('/home', 'HomeController@index');
-});
-*/
-
 
 Route::controller('activity', 'ActivityController');
 Route::controller('account', 'AccountController');
