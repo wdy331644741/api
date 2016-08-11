08a145362b9cd9eb4e85e6dc1ec7a29750de815d
zhaodongran <zhaodongran@wanglibank,com>
2016-08-11 16:19:09 +0800

    微信分享签名认证

--- lib/Weixin.php
+++ lib/Weixin.php
@@ -75,13 +75,24 @@ class Weixin
      */
 
     public function get_access_token(){
-        $access_token_url = "/cgi-bin/token?grant_type=client_credential&appid={$this->_appid}&secret={$this->_appsecret}";
-        $res = $this->_client->get($access_token_url);
-        if($res->getStatusCode() == 200){
-            $data = (array)json_decode($res->getBody());
-            return $data;
+        if (!Cache::has('wechat_access_token')) {
+            $access_token_url = "/cgi-bin/token?grant_type=client_credential&appid={$this->_appid}&secret={$this->_appsecret}";
+            $res = $this->_client->get($access_token_url);
+            if($res->getStatusCode() == 200){
+                $data = (array)json_decode($res->getBody());
+                if (isset($data['access_token'])){
+                    Cache::put('wechat_access_token',$data['access_token'],120);
+                    return $data['access_token'];
+                }else{
+                    file_put_contents(storage_path('logs/wechat_access_token_error_'.date('Y-m-d').'log'),date('Y-m-d H:i:s')."=> code:【".$data['errcode'].'】-errmsg：【'.$data['errmsg'].'】'.PHP_EOL,FILE_APPEND);
+                    return false;
+                }
+            }
+
+        }else{
+            return Cache::get('wechat_access_token');
         }
-        return false;
+
     }
 
 
@@ -94,15 +105,11 @@ class Weixin
      */
 
     public function send_template_msg($openid,$template_id,$data){
-        $access_token = '';
-        if (!Cache::has('wechat_access_token')) {
-            $wechat_arr = $this->get_access_token();
-            $access_token = $wechat_arr['access_token'];
-            Cache::put('wechat_access_token',$access_token,120);
-        }else{
-            $access_token = Cache::get('wechat_access_token');
+        $access_token_url = '';
+        $access_token = $this->get_access_token();
+        if($access_token){
+            $access_token_url = "/cgi-bin/message/template/send?access_token={$access_token}";
         }
-        $access_token_url = "/cgi-bin/message/template/send?access_token={$access_token}";
         //$access_token_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={$access_token}";
         $postData = array(
             'touser'=>$openid,
@@ -117,14 +124,72 @@ class Weixin
             if($result->errcode == 0){
                 return true;
             }else{
-                file_put_contents(storage_path('logs/send_template_msg-error'.date('Y-m-d').'.log'),date('y-m-d H:i:s').'：msgid：【'.$result->msgid.'】code:【'.$result->errcode.'】-errormsg:【'.$result->errmsg.'】'.PHP_EOL,FILE_APPEND);
+                file_put_contents(storage_path('logs/send_template_msg-error'.date('Y-m-d').'.log'),date('y-m-d H:i:s').'=> msgid：【'.$result->msgid.'】code:【'.$result->errcode.'】-errormsg:【'.$result->errmsg.'】'.PHP_EOL,FILE_APPEND);
                 return false;
             }
         }
         return false;
     }
 
+    /*
+     *通过当前url获取签名数组
+     * @params：string url
+     */
+    public function get_sign_package($url){
+        $timestamp = time();
+        $nonceStr = $this->createNonceStr();
+        $jsapiTicket = $this->get_js_apiticket();
+        $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+        $signature = sha1($string);
+        $signPackage = array(
+            "appId"     => env('WECHAT_APPID'),
+            "nonceStr"  => $nonceStr,
+            "timestamp" => $timestamp,
+            "signature" => $signature,
+        );
+        return $signPackage;
+    }
 
+
+    /*
+     *获取js_api_ticket
+     */
+    private function get_js_apiticket(){
+        if(!Cache::has('wechat_js_apiticket')){
+            $accessToken = $this->get_access_token();
+            $url = "/cgi-bin/ticket/getticket?type=jsapi&access_token=".$accessToken;
+            $res = $this->_client->get($url);
+            if($res->getStatusCode() == 200){
+                $data = (array)json_decode($res->getBody());
+                if (isset($data['ticket'])){
+                    Cache::put('wechat_js_apiticket',$data['ticket'],118);
+                    return $data['ticket'];
+                }else{
+                    file_put_contents(storage_path('logs/wechat_js_apiticket_error_'.date('Y-m-d').'log'),date('Y-m-d H:i:s')."=> code:【".$data['errcode'].'】-errmsg：【'.$data['errmsg'].'】'.PHP_EOL,FILE_APPEND);
+                    return false;
+                }
+            }
+        } else {
+            return Cache::get('wechat_js_apiticket');
+        }
+
+    }
+
+    /*
+     *生成随机字符串
+     */
+    private function createNonceStr($length = 16) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    /*
+     *CURL发送请求
+     */
     private function wx_request($url,$data=null){
         $curl = curl_init(); // 启动一个CURL会话
         curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
