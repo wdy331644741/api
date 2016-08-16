dabbc7e38c9e127f93bae00fd3b00f32c39cad69
zhaodongran <zhaodongran@wanglibank,com>
2016-08-16 10:07:25 +0800

    文章列表接口

--- app/Http/JsonRpcs/ContentJsonrpc.php
+++ app/Http/JsonRpcs/ContentJsonrpc.php
@@ -124,7 +124,7 @@ class ContentJsonRpc extends JsonRpc {
             return $page;
         });
         $pagenum = isset($params->pagenum) ? $params->pagenum : 10;
-        $data = Content::select('id','title','content','release_at')->where('type_id',$type_id)->orderByRaw('id + sort desc')->paginate($pagenum)->get();
+        $data = Content::select('id','title','content','release_at')->where('type_id',$type_id)->orderByRaw('id + sort desc')->paginate($pagenum);
 
         return array(
             'code' => 0,
