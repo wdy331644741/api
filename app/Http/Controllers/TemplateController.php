70499a5952b325ceb48f3956351e7056e175f54a
zhaodongran <zhaodongran@wanglibank,com>
2016-08-24 11:01:00 +0800

    生成常见问题HTML

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -129,9 +129,14 @@ class TemplateController extends Controller
     public function postHelpList() {
         $contentType = ContentType::where('alias_name','questions')->first();
         $typeArr = ContentType::select('id','parent_id','name')->where('parent_id',$contentType->id)->orderByRaw('id + sort DESC')->orderBy('id','desc')->get();
-        $typeId = ContentType::where('parent_id',$contentType->id)->lists('id')->toArray();
-        $data = ContentType::whereIn('id',$typeId)->with('contents')->orderByRaw('id + sort DESC')->orderBy('id','desc')->get()->toArray();
-        $res = view('static.help', array('data'=>$data,'types'=>$typeArr))->render();
+        $typeId = array();
+        foreach ($typeArr as $item){
+            $typeId[] = $item['id'];
+        }
+        //$typeId = ContentType::where('parent_id',$contentType->id)->lists('id')->toArray();
+        $data = ContentType::whereIn('id',$typeId)->with('contents')->orderByRaw('id + sort DESC')->orderBy('id','desc')->get();
+        $often = Content::select('id','type_id','title')->where(['release'=>1,'platform'=>1])->get();
+        $res = view('static.help', array('data'=>$data,'types'=>$typeArr,'oftens'=>$often))->render();
         Storage::disk('static')->put("help.html", $res);
     }
     #TODO //生成加入我们html
