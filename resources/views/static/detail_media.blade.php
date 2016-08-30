20fc91f62e2422e3cb56e8ee5e52795d5bbb9315
zhaodongran <zhaodongran@wanglibank,com>
2016-08-30 18:26:44 +0800

    添加模板模板

---
+++ resources/views/static/detail_media.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>{!! $title !!}-网利宝</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="baidu-site-verification" content="iBKXouRC1a"/>
+    <meta name="apple-itunes-app" content="app-id=881326898 affiliate-data=, app-argument="/>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link type="text/css" rel="styleSheet" href='/css/cms/public.css'>
+    <link type="text/css" rel="styleSheet" href='/css/cms/news_list.css'>
+    <link href="/css/icon/iconfont.css" rel="stylesheet" type="text/css"/>
+    <link href="/theme/about/css/pc.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/header-footer.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="sheader"></div>
+<div class="container">
+    <div class="about_sidebar clearfix">
+        <div class="about_sidebar_div"><a href="/cms/theme/about/" class="about_sidebar_a">关于我们</a><a href="/cms/news/list/1.html" class="about_sidebar_a active">媒体报道</a><a href="/cms/dynamic/list/1.html" class="about_sidebar_a">网利动态</a><a href="/cms/notice/list/1.html" class="">网站公告</a><!-- <a href="/hiring" class="">加入我们</a> -->
+        </div>
+    </div>
+    <div class='section'>
+        <div class="page_nav">
+            <a href="../list/1.html" target="_blank">媒体报道</a>&gt;报道详情
+        </div>
+        <div class='media_box detail_box clearfix'>
+            <h3>{!! $title !!}</h3>
+            <div class="media_coverage_time">{!! date('Y-m-d H:i',strtotime($release_at)) !!}</div>
+            <div class='medil_detail'>
+                {!! $content !!}
+            </div>
+        </div>
+    </div>
+</div>
+<div id="sfooter"></div>
+<script type="text/javascript" src="https://php1.wanglibao.com/js/sea.js"></script>
+<script type="text/javascript" src="https://php1.wanglibao.com/js/sea-config.js"></script>
+<script type="text/javascript">
+seajs.use(['jquery'],function($){
+    seajs.use(['public', 'template', 'header'],function(){
+    });
+});
+</script>
+</body>
+</html>
---
+++ resources/views/static/detail_notice.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>{!! $title !!}-网利宝</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="baidu-site-verification" content="iBKXouRC1a"/>
+    <meta name="apple-itunes-app" content="app-id=881326898 affiliate-data=, app-argument="/>
+    <meta name="keywords" content="网利宝,网利宝官网,p2p金融,p2p理财,p2p网贷,P2p贷款,p2p平台,P2P网贷平台,互联网金融">
+    <meta name="description" content="网利宝（www.wanglibao.com）是中国领先的互联网金融p2p理财、p2p网贷平台，网利宝官网提供安全、精准的网贷平台投资及互联网金融服务，大型机构100%本息担保，保障投资人资金安全，P2P投资理财网贷客户首选的互联网金融平台。">
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link type="text/css" rel="styleSheet" href='/css/cms/public.css'>
+    <link type="text/css" rel="styleSheet" href='/css/cms/news_list.css'>
+    <link href="/css/icon/iconfont.css" rel="stylesheet" type="text/css"/>
+    <link href="/theme/about/css/pc.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/header-footer.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="sheader"></div>
+<div class="container">
+    <div class="about_sidebar clearfix">
+        <div class="about_sidebar_div"><a href="/cms/theme/about/" class="about_sidebar_a">关于我们</a><a href="/cms/news/list/1.html" class="about_sidebar_a">媒体报道</a><a href="/cms/dynamic/list/1.html" class="about_sidebar_a">网利动态</a><a href="/cms/notice/list/1.html" class="active">网站公告</a><!-- <a href="/hiring" class="">加入我们</a> --></div>
+    </div>
+    <div class='section'>
+        <div class="page_nav">
+            <a href="../list/1.html" target="_blank">网站公告</a>&gt;公告详情
+        </div>
+        <div class='media_box detail_box clearfix'>
+            <div class='announcement_detail clearfix'>
+                <h3>{!! $title !!}</h3>
+                <span>{!! date('Y-m-d',strtotime($release_at)) !!}</span>
+            </div>
+            <div class='medil_detail'>
+                {!! $content !!}
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div id="sfooter"></div>
+<script type="text/javascript" src="https://php1.wanglibao.com/js/sea.js"></script>
+<script type="text/javascript" src="https://php1.wanglibao.com/js/sea-config.js"></script>
+<script src='../../js/jquery.min.js'></script>
+<script src='../../js/news_list.js'></script>
+<script type="text/javascript">
+seajs.use(['jquery'],function($){
+    seajs.use(['public', 'template', 'header'],function(){
+    });
+});
+</script>
+</body>
+</html>
