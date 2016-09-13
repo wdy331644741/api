e095113ed50701ff4e13ec9fb97396a84cfe6e8c
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-09-13 14:54:25 +0800

    jsonrpc发奖

---
+++ app/Http/JsonRpcs/InsideJsonrpc.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\JsonRpcs;
+use App\Exceptions\OmgException;
+use App\Service\SendAward;
+class InsideJsonrpc extends JsonRpc {
+    
+    /**
+     *  发送奖品
+     *
+     * @JsonRpcMethod
+     */
+    public function sendAward($params) {
+        $userId = intval($params->userId);
+        $awardType = intval($params->awardType);
+        $awardId = intval($params->awardId);
+        $sourceName = trim($params->sourceName);
+        if(empty($userId) || empty($awardType) || empty($awardId) || empty($sourceName)){
+            throw new OmgException(OmgException::PARAMS_NEED_ERROR);
+        }
+        $res = SendAward::sendDataRole($userId, $awardType, $awardId, 0, $sourceName);
+        return array(
+            'code' => 0,
+            'message' => 'success',
+            'data' => $res,
+        );
+    }
+}
