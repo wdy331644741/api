91b84656f150d1914cb725101eb8f59494a1bd8e
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-09-13 17:14:09 +0800

    补发修改

--- app/Jobs/ReissueAward.php
+++ app/Jobs/ReissueAward.php
@@ -23,6 +23,7 @@ class ReissueAward extends Job implements ShouldQueue
         //获取出发送失败的奖品列表
         $unSendList = SendRewardLog::where('status',0)->get()->toArray();
         //循环发奖
+        $i = 0;
         foreach($unSendList as $item){
             if(empty($item['award_id']) || empty($item['award_type']) || empty($item['user_id']) || empty($item['id']) || empty($item['activity_id'])){
                 continue;
@@ -31,6 +32,10 @@ class ReissueAward extends Job implements ShouldQueue
             $activityName = isset($activityName[0]['name']) ? $activityName[0]['name'] : '';
             if(empty($activityName)){
                 continue;
+            }
+            $i++;
+            if($i >= 100){
+                return '完成';
             }
             SendAward::sendDataRole($item['user_id'], $item['award_type'], $item['award_id'], $item['activity_id'], $activityName,0,$item['id']);
         }
