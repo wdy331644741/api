c559c529a772c54b24ea051e4c32db2f3a68045b
zhaodongran <zhaodongran@wanglibank,com>
2016-09-18 18:16:30 +0800

    删除人数上限限制

---
+++ database/migrations/2016_09_18_181501_del_joinmax_to_activities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DelJoinmaxToActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropColumn('join_max');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            //
+        });
+    }
+}
