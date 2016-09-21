a1bdb16e83fdcb34b11b45a93d1fd17f082cd8aa
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-09-21 10:17:59 +0800

    果粉专享修改

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -33,6 +33,7 @@ class MarkController extends Controller
                 $fileName = date("YmdHis").mt_rand(1000,9999);
                 $typeName = "xls";
                 $cellData = $list['result']['data'];
+
                 foreach($cellData as $key => $item){
                     if($key == 0){
                         $cellData[$key] = array('散标id','用户id','投资金额','期限','标类型','天标或月标');
@@ -43,10 +44,10 @@ class MarkController extends Controller
                     if($item['type'] == 20){
                         $type = "散标";
                     }
-                    if($item['type'] == 3){
+                    if($item['scatter_type'] == 1){
                         $scatter_type = "天标";
                     }
-                    if($item['type'] == 20){
+                    if($item['scatter_type'] == 2){
                         $scatter_type = "月标";
                     }
                     $cellData[$key+1] = array($item['project_id'],$item['user_id'],$item['amount'],$item['term'],$type,$scatter_type);
