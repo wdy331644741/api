4e3e8ec56a20ae7ebb7db0c62e7dd643f0c4c606
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-10-19 11:58:59 +0800

    获取金锤数接口修改

--- app/Http/JsonRpcs/CouponCountJsonrpc.php
+++ app/Http/JsonRpcs/CouponCountJsonrpc.php
@@ -42,26 +42,31 @@ class CouponCountJsonRpc extends JsonRpc {
 
         $id = array_column($award_ids, 'id');
         //获取最新收到金锤的用户id
-        $user_id = SendRewardLog::where('award_type',6)->whereIn('award_id',$id)->where('status','>=',1)->select('user_id')->orderBy('id', 'desc')->first()->toArray();
+        $user_id = SendRewardLog::where('award_type',6)->whereIn('award_id',$id)->where('status','>=',1)->select('user_id')->orderBy('id', 'desc')->take(50)->get()->toArray();
         if(empty($user_id)){
             throw new OmgException(OmgException::NO_DATA);
         }
+        $user_id = array_unique(array_column($user_id, 'user_id'));
         //获取该用户收到多少金锤
-        $count = SendRewardLog::where('award_type',6)->whereIn('award_id',$id)->where('status','>=',1)->where('user_id',$user_id['user_id'])->count();
-        //根据用户ID获取真实姓名
-        $url = env('INSIDE_HTTP_URL');
-        $client = new JsonRpcClient($url);
-        $user_id = $user_id['user_id'];
-        if(empty($user_id)){
-            throw new OmgException(OmgException::NO_DATA);
+        foreach($user_id as $uid){
+            $count = SendRewardLog::where('award_type',6)->whereIn('award_id',$id)->where('status','>=',1)->where('user_id',$uid)->count();
+            //根据用户ID获取真实姓名
+            $url = env('INSIDE_HTTP_URL');
+            $client = new JsonRpcClient($url);
+            if(empty($uid)){
+                throw new OmgException(OmgException::NO_DATA);
+            }
+            $userBase = $client->userBasicInfo(array('userId'=>$uid));
+            $phone = isset($userBase['result']['data']['phone']) ? $userBase['result']['data']['phone'] : '';
+            if(!empty($phone)){
+                $phone = substr_replace($phone, '*****', 3, 5);
+            }
+            $data[] = $phone." 获得金锤码".$count."个！";
         }
-        $userBase = $client->userBasicInfo(array('userId'=>$user_id));
-        $realName = isset($userBase['result']['data']['realname']) ? $userBase['result']['data']['realname'] : '';
-        $message = $realName."获得金锤码".$count."个！";
         return array(
             'code' => 0,
             'message' => 'success',
-            'data' => array('message'=>$message)
+            'data' => $data
         );
     }
 }
