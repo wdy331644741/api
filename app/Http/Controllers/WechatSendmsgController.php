19805488c155fbcceedeb511d8039f62e4c67b13
zhaodongran <zhaodongran@wanglibank,com>
2016-11-15 16:18:19 +0800

    微信推送消息

--- app/Http/Controllers/WechatSendmsgController.php
+++ app/Http/Controllers/WechatSendmsgController.php
@@ -12,31 +12,32 @@ class WechatSendmsgController extends Controller
 {
     public function postSendmsg(Request $request){
         $tag = $request->tag;
-        if($tag !== 'wechat_msg'){
+        if($tag !== 'wechatMSG'){
             return array('error_code'=>10000, 'data'=>array('error_msg'=>'通知错误'));
         }
-        $type = $request->type;
-        $userId = isset($request->user_id) ? intval($request->user_id) : 0;
+        $requests = $request->all();
+        $type = $requests['mtype'];
+        $userId = isset($requests['args']['user_id']) ? intval($requests['args']['user_id']) : 0;
         if(!$type || !$userId ){
             return array('error_code'=>10001, 'data'=>array('error_msg'=>'参数错误'));
         }
-        /*$client = new JsonRpcClient(env('ACCOUNT_HTTP_URL'));
-        $res = $client->getOpenId($userId,'wechat');
+        $client = new JsonRpcClient(env('ACCOUNT_HTTP_URL'));
+        $res = $client->accountOpenId($userId,'wechat');
         if(isset($res['error'])){
-            return '获取openid出现错误';
-        }*/
-        $openId = 'ovewut6VpqDz6ux4nJg2cKx0srh0';//$res['result']['data']['openid'];
+            return array('error_code'=>10000, 'data'=>array('error_msg'=>'获取openid出现错误'));
+        }
+        $openId = $res['result']['data']['openid'];//'ovewut6VpqDz6ux4nJg2cKx0srh0';
         switch ($type){
             case 'recharge_success' :
-                $res = $this->_sendRecharge($openId,$request->all());
+                $res = $this->_sendRecharge($openId,$requests);
                 return response()->json($res);
                 break;
-            case 'present_success' :
-                $res = $this->_sendPresent($openId,$request->all());
+            case 'withdraw_success' :
+                $res = $this->_sendWithdraw($openId,$requests);
                 return response()->json($res);
                 break;
-            case 'payment_account' :
-                $res = $this->_sendPayment($openId,$request->all());
+            case 'invest_success' :
+                $res = $this->_sendInvest($openId,$requests);
                 return response()->json($res);
                 break;
             default :
@@ -47,16 +48,16 @@ class WechatSendmsgController extends Controller
 
     //发送充值成功消息
     private function _sendRecharge($openId,$data){
-        if(!$data['real_name'] || !$data['money']){
+        if(!$data['args']['username'] || !$data['args']['amount']){
             return array('error_code'=>10001, 'data'=>array('error_msg'=>'参数错误'));
         }
         $postData = array(
             'first'=>array(
-                'value'=>'亲爱的'.$data['real_name'].'，您已充值成功。',
+                'value'=>'亲爱的'.$data['args']['username'].'，您已充值成功。',
                 'color'=>'#000000'
             ),
             'keyword1'=>array(
-                'value'=>$data['money'].'元',
+                'value'=>$data['args']['amount'].'元',
                 'color'=>'#173177'
             ),
             'remark'=>array(
@@ -75,22 +76,20 @@ class WechatSendmsgController extends Controller
         }elseif ($res['errcode'] == 40001){
             return array('error_code'=> 40001, 'data'=>array('error_msg'=>'access_token授权失败'));
         }
-
-
     }
 
     //发送提现成功消息
-    private function _sendPresent($openId,$data){
-        if(!$data['real_name'] || !$data['money']){
+    private function _sendWithdraw($openId,$data){
+        if(!$data['args']['username'] || !$data['args']['amount']){
             return array('error_code'=>10001, 'data'=>array('error_msg'=>'参数错误'));
         }
         $postData = array(
             'first'=>array(
-                'value'=>'亲爱的'.$data['real_name'].'，您的提现已成功。',
+                'value'=>'亲爱的'.$data['args']['username'].'，您的提现已成功。',
                 'color'=>'#000000'
             ),
             'keyword1'=>array(
-                'value'=>$data['money'].'元',
+                'value'=>$data['args']['amount'].'元',
                 'color'=>'#173177'
             ),
             'remark'=>array(
@@ -98,7 +97,7 @@ class WechatSendmsgController extends Controller
                 'color'=>'#000000'
             )
         );
-        $template_id = Config::get('open.weixin.msg_template.present_success');
+        $template_id = Config::get('open.weixin.msg_template.withdraw_success');
         $weixin = new Weixin();
         $res = $weixin->send_template_msg($openId,$template_id,$postData);
         if($res === false) {
@@ -112,22 +111,22 @@ class WechatSendmsgController extends Controller
     }
 
 
-    //发送提现成功消息
-    private function _sendPayment($openId,$data){
-        if(!$data['real_name'] || !$data['money'] || !$data['project_name']){
+    //发送回款成功消息
+    private function _sendInvest($openId,$data){
+        if(!$data['args']['username'] || !$data['args']['money'] || !$data['args']['project_name']){
             return array('error_code'=>10001, 'data'=>array('error_msg'=>'参数错误'));
         }
         $postData = array(
             'first'=>array(
-                'value'=>'亲爱的'.$data['real_name'].'，您投资的项目收到还款，已到账',
+                'value'=>'亲爱的'.$data['args']['username'].'，您投资的项目收到还款，已到账',
                 'color'=>'#000000'
             ),
             'keyword1'=>array(
-                'value'=>$data['project_name'],
+                'value'=>$data['args']['project_name'],
                 'color'=>'#173177'
             ),
             'keyword2'=>array(
-                'value'=>$data['money'].'元',
+                'value'=>$data['args']['money'].'元',
                 'color'=>'#173177'
             ),
             'remark'=>array(
@@ -135,7 +134,7 @@ class WechatSendmsgController extends Controller
                 'color'=>'#000000'
             )
         );
-        $template_id = Config::get('open.weixin.msg_template.payment_account');
+        $template_id = Config::get('open.weixin.msg_template.invest_success');
         $weixin = new Weixin();
         $res = $weixin->send_template_msg($openId,$template_id,$postData);
         if($res === false) {
