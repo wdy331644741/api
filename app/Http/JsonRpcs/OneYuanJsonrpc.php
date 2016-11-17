6f1ee403e41d2c3ec5f0e9d725748522bd292bf4
ranhaiqiang <ranhaiqiang@wanglibank.com>
2016-11-17 15:33:07 +0800

    一元夺宝接口上传

--- app/Http/JsonRpcs/OneYuanJsonrpc.php
+++ app/Http/JsonRpcs/OneYuanJsonrpc.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\JsonRpcs;
 
-use App\Models\OneYuan;
+
 use App\Models\OneYuanBuyInfo;
 use App\Exceptions\OmgException;
 use Lib\JsonRpcClient;
 use App\Service\SendAward;
+use App\Service\OneYuanBasic;
+use App\Models\OneYuan;
 
 class OneYuanJsonRpc extends JsonRpc {
 
@@ -15,12 +17,7 @@ class OneYuanJsonRpc extends JsonRpc {
      *
      * @JsonRpcMethod
      */
-    public function oneYuanMallList($params) {
-        $where = array();
-        $where['groups'] = trim($params->groups);
-        if(empty($where['groups'])){
-            throw new OmgException(OmgException::PARAMS_NEED_ERROR);
-        }
+    public function oneYuanMallList() {
         $where['status'] = 1;
         $list = OneYuan::where($where)->orderBy('exhibition','asc')->get()->toArray();
         return array(
@@ -31,71 +28,65 @@ class OneYuanJsonRpc extends JsonRpc {
     }
 
     /**
+     *  购买积分
+     *
+     * @JsonRpcMethod
+     */
+    public function buyLuckNum($params) {
+        $userId = intval($params->userId);
+        $num = intval($params->num);
+        if(empty($userId) || empty($num)){
+            throw new OmgException(OmgException::PARAMS_NEED_ERROR);
+        }
+        //调用孙峰接口余额购买次数
+        $url = env('INSIDE_HTTP_URL');
+        $client = new JsonRpcClient($url);
+        $status = $client->userBasicInfo(array('userId' =>$userId));
+        $status = 1;
+        if($status) {
+            //如果成功
+            $return = OneYuanBasic::addNum($userId,$num);
+            if(isset($return['status']) && $return['status'] === true){
+                return array(
+                    'code' => 0,
+                    'message' => 'success'
+                );
+            }
+            return array(
+                'code' => -1,
+                'message' => 'fail'
+            );
+        }
+        //如果失败
+        return array(
+            'code' => -1,
+            'message' => 'fail'
+        );
+    }
+    /**
      * 参与抽奖
      * @JsonRpcMethod
      */
     public function oneYuanJoin($params) {
         $userId = intval($params->userId);
         $mallId = intval($params->mallId);
-        if(empty($userId) || empty($mallId)){
+        $num = intval($params->num);
+        if(empty($userId) || empty($mallId) || empty($num)){
             throw new OmgException(OmgException::PARAMS_NEED_ERROR);
         }
-        //获取用户的积分额
-        $url = env('INSIDE_HTTP_URL');
-        $client = new JsonRpcClient($url);
-        $userBase = $client->userBasicInfo(array('userId' =>$userId));
-        $integralTotal = isset($userBase['result']['data']['integral']) ? $userBase['result']['data']['integral'] : 1000;
-        //判断积分值够不够买该奖品
-        $where = array();
-        $where['id'] = $mallId;
-        $where['status'] = 1;
-        $data = OneYuan::where($where)->get()->toArray();
-        //判断数据是否存在
-        if(empty($data)){
-            throw new OmgException(OmgException::NO_DATA);
-        }
-        $data = isset($data[0]) ? $data[0] : array();
-        if(empty($data)){
-            throw new OmgException(OmgException::NO_DATA);
-        }
-        //判断值是否有效
-        if($data['integral'] < 1){
-            throw new OmgException(OmgException::INTEGRAL_FAIL);
-        }
-        //判断该用户是否超过了购买
-        $whereEX = array();
-        $whereEX['user_id'] = $userId;
-        $whereEX['mall_id'] = $mallId;
-        $whereEX['send_status'] = 1;
-        $count = OneYuanExchange::where($whereEX)->count();
-        if($data['user_quantity'] != 0 &&$count >= $data['user_quantity']){
-            throw new OmgException(OmgException::EXCEED_FAIL);
-        }
-        //如果花费大于于拥有的总积分
-        if($data['integral'] > $integralTotal) {
-            throw new OmgException(OmgException::INTEGRAL_LACK_FAIL);
-        }
-        //插入交易日志
-        $insert = array();
-        $insert['user_id'] = $userId;
-        $insert['mall_id'] = $mallId;
-        $insert['snapshot'] = json_encode($data);
-        $insert['send_status'] = 0;
-        //发送奖品
-        $return = SendAward::sendDataRole($userId,$data['award_type'],$data['award_id'],0,'积分兑换',0,0);
-        if($return['status'] === true){
-            //调用孙峰接口减去积分
-
-            //修改发送成功人数+1
-            OneYuan::where($where)->increment('send_quantity');
-            $insert['send_status'] = 1;
-        }
-        $id = OneYuanExchange::insertGetId($insert);
-        if($id && $insert['send_status'] == 1){
-            return array(
-                'code' => 0,
-                'message' => 'success'
-            );
+        //获取用户的抽奖次数判断是否够用
+        $totalNum = OneYuanBasic::getUserLuckNum($userId);
+        if(isset($totalNum['status']) && !empty($totalNum['status'])){
+            if($totalNum['status'] >= $num){
+                //添加到抽奖记录表中
+                $return = OneYuanBasic::insertBuyInfo($userId,$mallId,$num);
+                if(isset($return['status']) && $return['status'] === true){
+                    return array(
+                        'code' => 0,
+                        'message' => 'success'
+                    );
+                }
+            }
         }
         return array(
             'code' => -1,
---
+++ app/Service/OneYuanBasic.php
@@ -0,0 +1,110 @@
+<?php
+namespace App\Service;
+use App\Models\OneYuanUserInfo;
+use App\Models\OneYuanBuyInfo;
+class OneYuanBasic
+{
+    /**
+     * 给用户添加抽奖次数
+     * @param $userId
+     * @param $num
+     * @return array
+     */
+    static function addNum($userId,$num){
+        if(empty($userId) || empty($num)){
+            return array("status"=>false,"msg"=>"参数有误");
+        }
+        $count = OneYuanUserInfo::where("user_id",$userId)->count();
+        //判断是否存在
+        if(!$count){
+            //插入一条数据
+            $data = array();
+            $data['user_id'] = $userId;
+            $data['num'] = $num;
+            $data['updated_at'] = date("Y-m-d H:i:s");
+            $data['created_at'] = date("Y-m-d H:i:s");
+            $id = OneYuanUserInfo::insertGetId($data);
+            if($id){
+                return array("status"=>true,"msg"=>"添加成功","data"=>$id);
+            }
+            return array("status"=>true,"msg"=>"添加失败");
+        }
+        $status = OneYuanUserInfo::where('user_id',$userId)->increment('num', $num,array('updated_at'=>date("Y-m-d H:i:s")));
+        if($status){
+            return array("status"=>true,"msg"=>"添加成功","data"=>$status);
+        }
+        return array("status"=>true,"msg"=>"添加失败");
+    }
+    /**
+     * 给用户添加抽奖次数
+     * @param $userId
+     * @param $num
+     * @return array
+     */
+    static function reduceNum($userId,$num){
+        if(empty($userId) || empty($num)){
+            return array("status"=>false,"msg"=>"参数有误");
+        }
+        $count = OneYuanUserInfo::where("user_id",$userId)->count();
+        //判断是否存在
+        if(!$count){
+            return array("status"=>true,"msg"=>"该用户没有抽奖次数");
+        }
+        $status = OneYuanUserInfo::where('user_id',$userId)->decrement('num', $num,array('updated_at'=>date("Y-m-d H:i:s")));
+        if($status){
+            return array("status"=>true,"msg"=>"扣除抽奖次数成功","data"=>$status);
+        }
+        return array("status"=>true,"msg"=>"扣除抽奖次数失败");
+    }
+    /**
+     * 获取该用户的抽奖次数
+     * @param $userID
+     * @param $template
+     * @param $arr
+     * @return bool
+     */
+    static function getUserLuckNum($userId){
+        if(empty($userId)){
+            return array("status"=>false,"msg"=>"参数有误");
+        }
+        //获取用户抽奖次数
+        $data = OneYuanUserInfo::where("user_id",$userId)->first();
+        $num = isset($data['num']) ? $data['num'] : 0;
+        return array("status"=>true,"msg"=>"获取成功","data"=>$num);
+    }
+    /**
+     * 添加到抽奖记录表中
+     * @param $userID,$mallId,$num
+     * @param $template
+     * @param $arr
+     * @return bool
+     */
+    static function insertBuyInfo($userId,$mallId,$num){
+        if(empty($userId) || empty($mallId) || empty($num)){
+            return array("status"=>false,"msg"=>"参数有误");
+        }
+        //获取该商品的最大值
+        $max = OneYuanBuyInfo::where("mall_id",$mallId+1)->select('end')->orderBy("end","desc")->first();
+        $end = isset($max['end']) ? $max['end'] : 0;
+        //添加的数据
+        $data = array();
+        $data['user_id'] = $userId;
+        $data['mall_id'] = $mallId;
+        $data['num'] = $num;
+        $data['buy_time'] = time();
+        $data['created_at'] = date("Y-m-d H:i:s");
+        $data['updated_at'] = date("Y-m-d H:i:s");
+        $data['start'] = $end+1;
+        $data['end'] = $end+$num;
+        if(empty($max)){
+            $data['start'] = 1;
+            $data['end'] = $num;
+        }
+        $id = OneYuanBuyInfo::insertGetId($data);
+        if($id){
+            return array("status"=>true,"msg"=>"抽奖成功","data"=>$id);
+        }
+        return array("status"=>false,"msg"=>"抽奖失败");
+    }
+
+}
