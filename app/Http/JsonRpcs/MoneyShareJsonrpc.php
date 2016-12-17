d10123d3b44659c814ee9a64d5cf111a6095ca43
wangning <wangning@wanglibank.com>
2016-12-17 18:09:36 +0800

    红包发送体验金逻辑重构

--- app/Http/JsonRpcs/MoneyShareJsonrpc.php
+++ app/Http/JsonRpcs/MoneyShareJsonrpc.php
@@ -5,7 +5,8 @@ namespace App\Http\JsonRpcs;
 use App\Exceptions\OmgException;
 use App\Service\MoneyShareBasic;
 use Illuminate\Support\Facades\Crypt;
-
+use App\Models\MoneyShare;
+use App\Models\MoneyShareInfo;
 
 class MoneyShareJsonRpc extends JsonRpc {
 
@@ -16,6 +17,7 @@ class MoneyShareJsonRpc extends JsonRpc {
      */
     public function moneyShareSendAward($params) {
         global $userId;
+        $userId = 1;
         if(empty($userId)){
             throw new OmgException(OmgException::NO_LOGIN);
         }
@@ -25,24 +27,65 @@ class MoneyShareJsonRpc extends JsonRpc {
         }
         //解密identify
         $identify = Crypt::decrypt(urldecode($identify));
-        //发送体验金
-        $data = MoneyShareBasic::sendMoney($identify,$userId);
-        if(isset($data['status']) && $data['status'] === false){
-            if($data['code'] === -1){
-                throw new OmgException(OmgException::MALL_NOT_EXIST);
-            }elseif($data['code'] === -2){
-                throw new OmgException(OmgException::MALL_IS_HAS);
-            }elseif($data['code'] === -3){
-                throw new OmgException(OmgException::DATA_ERROR);
-            }elseif($data['code'] === -4){
-                throw new OmgException(OmgException::AWARD_NOT_EXIST);
-            }elseif($data['code'] === -5){
-                throw new OmgException(OmgException::SEND_ERROR);
+        
+        $result = ['award' => 0, 'isGot' => false, 'mall' =>[] , 'recentList' => [], 'topList' => []];
+        
+        
+        // 商品是否存在
+        $mallInfo = MoneyShare::where(['identify' => $identify, 'status' => 1])->lockForUpdate()->first();
+        if(!$mallInfo){
+            throw new OmgException(OmgException::ACTIVITY_NOT_EXIST);
+        }
+        $result['recentList'] = MoneyShareInfo::where('main_id', $mallInfo['id'])->orderBy('id', 'desc')->take(50)->get();
+        $result['topList'] = MoneyShareInfo::where('main_id', $mallInfo['id'])->orderBy('money', 'desc')->take(50)->get();
+        
+        $result['mall'] = $mallInfo;
+        
+        // 计算剩余金额和剩余数量
+        $remain = $mallInfo->total_money - $mallInfo->use_money;
+        $remain = $remain > 0 ? $remain : 0;
+        $remainNum = $mallInfo->total_num - $mallInfo->receive_num;
+        $remainNum = $remainNum > 0 ? $remainNum : 0;
+        
+        //用户领取过
+        $join = MoneyShareInfo::where(['user_id' => $userId, 'main_id' => $mallInfo->id])->first();
+        if($join){
+            $result['isGot'] = true;       
+            $result['award'] = $join['money'];
+            return $result;
+        }
+        
+        // 发体验金
+        if(!$result['isGot']) {
+            $money = MoneyShareBasic::getRandomMoney($remain,$remainNum,$mallInfo->min,$mallInfo->max);
+            $mallInfo->increment('use_money', $money);
+            $mallInfo->increment('receive_num', 1);
+
+            //发送体验金
+            $expRes = MoneyShareBasic::sendAward($userId, $mallInfo['award_type'], $mallInfo['award_id'], $money, $mallInfo['id']);
+            MoneyShareInfo::create([
+                'user_id' => $userId,
+                'main_id' => $expRes['award']['main_id'],
+                'uuid' => $expRes['award']['uuid'],
+                'money' => $money,
+                'source_id' => $expRes['award']['source_id'],
+                'award_type' => $mallInfo['award_type'],
+                'award_id' => $mallInfo['award_id'],
+                'remark' => json_encode($expRes['remark'], JSON_UNESCAPED_UNICODE),
+                'mail_status' => $expRes['mail_status'],
+                'message_status' => $expRes['message_status'],
+                'status' => $expRes['status'],
+            ]);
+            if(!$expRes['status']) {
+                throw new OmgException(OmgException::API_FAILED);
             }
         }
+
+        
         return array(
             'code' => 0,
-            'message' => 'success'
+            'message' => 'success',
+            'data' => $result
         );
     }
 }
