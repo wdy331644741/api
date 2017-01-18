8a0c365e96cf9999212c39dc14f8dd4b58f955c3
zhaodongran <zhaodongran@wanglibank,com>
2017-01-18 14:25:33 +0800

    理财课堂列表详情页动态调用

---
+++ resources/views/content/detail_classroom.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>{!! $title !!}-网利宝</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="baidu-site-verification" content="iBKXouRC1a"/>
+    <meta name="apple-itunes-app" content="app-id=881326898 affiliate-data=, app-argument="/>
+    <meta name="keywords" content="@if($keywords != null) {!! $keywords !!} @endif">
+    <meta name="description" content="@if($description != null) {!! $description !!} @endif">
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <link type="text/css" rel="styleSheet" href='/css/cms/public.css'>
+    <link type="text/css" rel="styleSheet" href='/css/cms/news_list.css'>
+    <link href="/css/icon/iconfont.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/pc.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/header-footer.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="sheader"></div>
+<div class="container">
+    <div class="about_sidebar clearfix">
+        <div class="about_sidebar_div"><a href="/theme/about/" class="about_sidebar_a">关于我们</a><a href="{!! action('ContentController@getList',['type'=>'report']) !!}" class="about_sidebar_a">媒体报道</a><a href="{!! action('ContentController@getList',['type'=>'trends']) !!}" class="about_sidebar_a">网利动态</a><a href="{!! action('ContentController@getList',['type'=>'notice']) !!}" class="about_sidebar_a">网站公告</a><a href="{!! action('ContentController@getList',['type'=>'classroom']) !!}" class="active">理财课堂</a>
+    </div>
+    <div class='section'>
+        <div class="page_nav">
+            <a href="{!! action('ContentController@getList',['type'=>'classroom']) !!}" target="_blank">理财课堂</a>&gt;课程详情
+        </div>
+        <div class='media_box detail_box clearfix'>
+            <div class='announcement_detail clearfix'>
+                <h3>{!! $title !!}</h3>
+                {{--<span>{!! date('Y-m-d H:i',strtotime($release_at)) !!}</span>--}}
+            </div>
+
+            <div class='medil_detail'>
+                {!! $content !!}
+            </div>
+        </div>
+    </div>
+</div>
+<div id="sfooter"></div>
+<script type="text/javascript" src="/js/sea.js"></script>
+<script type="text/javascript" src="/js/sea-config.js"></script>
+<script type="text/javascript">
+    seajs.use(['jquery'],function($){
+        seajs.use(['public','template'],function(){
+            seajs.use(['header'],function($){
+
+            });
+        });
+    });
+</script>
+</body>
+</html>
