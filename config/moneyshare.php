286e46fea37c72e2d0c1f636134aac0ac46efe81
ranhaiqiang <ranhaiqiang@wanglibank.com>
2017-02-10 18:17:37 +0800

    红包分享二期

---
+++ config/moneyshare.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'user_red_num' => 5,//用户红包分享个数
+    'user_red_min' => 666,//用户红包分享最小值
+    'user_red_title' => "好友分享",//用户红包分享最小值
+    'user_red_content' => "该链接是由网利宝好友分享过来，感谢参与",//用户红包分享最小值
+];
+
