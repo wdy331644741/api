210556b05c5863d6a5f072d04612139e2ae57ee4
shihe <shihe@wanglibank.com>
2017-04-19 16:05:42 +0800

    用户消息

--- app/Models/Bbs/Pm.php
+++ app/Models/Bbs/Pm.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Pm extends Model
 {
     protected $table = 'bbs_pms';
+
+    public function comments(){
+        return $this->hasOne('App\Models\Bbs\Comment','id','cid');
+    }
+
+    public function threads(){
+        return $this->hasOne('App\Models\Bbs\Thread','id','tid');
+    }
+
+    public function fromUsers(){
+        return $this->hasOne('App\Models\Bbs\User','user_id','from_user_id');
+    }
+
 }
