e91238419f76c60682e3c199442a5632bf3fe688
wangning <wangning@wanglibank.com>
2017-05-04 14:53:19 +0800

    增加datatables

--- app/Models/Bbs/User.php
+++ app/Models/Bbs/User.php
@@ -8,6 +8,8 @@ class User extends Model
 {
     protected $table = 'bbs_users';
 
+    protected $guarded = ['created_at','updated_at'];
+
     public function threads(){
         return $this->hasMany('App\Models\Bbs\Thread','user_id','user_id');
     }
