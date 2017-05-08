068b6986c76bf194a3c5b27d1af228164769f4d9
zhaodongran <zhaodongran@wanglibank,com>
2017-05-08 16:48:43 +0800

    添加消息类型字段

--- app/Http/Controllers/Bbs/ThreadController.php
+++ app/Http/Controllers/Bbs/ThreadController.php
@@ -144,15 +144,15 @@ class ThreadController extends Controller
     public function postDel(Request $request){
         $validator = Validator::make($request->all(), [
             'id'=>'required|exists:bbs_threads,id',
-            'user_id'=>'required|exists:bbs_users,user_id',
             'cid'=>'required|exists:bbs_replay_configs,id'
         ]);
         if($validator->fails()){
             return $this->outputJson(10001,array('error_msg'=>$validator->errors()->first()));
         }
+        $user_id = Thread::find($request->id)->value('user_id');
         Thread::destroy($request->id);
         $pm = new Pm();
-        $pm->user_id = $request->user_id;
+        $pm->user_id = $user_id;
         $pm->from_user_id = 0;
         $pm->tid = $request->id;
         $pm->cid = $request->cid;
