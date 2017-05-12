75962c107eec26ea8cd2756bdc4d0c2f18967350
zhaodongran <zhaodongran@wanglibank,com>
2017-05-12 14:20:58 +0800

    社区数据库编码修改

--- database/migrations/2017_04_11_160951_create_bbs_threads_table.php
+++ database/migrations/2017_04_11_160951_create_bbs_threads_table.php
@@ -13,6 +13,8 @@ class CreateBbsThreadsTable extends Migration
     public function up()
     {
         Schema::create('bbs_threads', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collate = 'utf8mb4_unicode_ci';
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('type_id');
--- database/migrations/2017_04_13_170817_create_bbs_comments_table.php
+++ database/migrations/2017_04_13_170817_create_bbs_comments_table.php
@@ -13,6 +13,8 @@ class CreateBbsCommentsTable extends Migration
     public function up()
     {
         Schema::create('bbs_comments', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collate = 'utf8mb4_unicode_ci';
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('tid')->comment('帖子id');
