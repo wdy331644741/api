9c977663ac32a92ad3544b7a3ac5933cc014adfa
wangning <wangning@wanglibank.com>
2017-05-16 10:15:13 +0800

    518理财节代码入库

---
+++ app/Models/FiveOneEight.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FiveOneEight extends Model
+{
+    public $table = 'fiveoneeight';
+    protected $guarded = ['created_at', 'update_at'];
+}
