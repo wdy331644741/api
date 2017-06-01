4302f24efed061731b1d968da702f5eef56b3744
zhaodongran <zhaodongran@wanglibank,com>
2017-06-01 10:24:35 +0800

    Model

---
+++ app/Models/TmpWechatUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TmpWechatUser extends Model
+{
+    protected  $table = 'tmp_wecaht_users';
+    protected $guarded = ['created_at','updated_at'];
+}
