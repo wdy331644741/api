f7d5eed113b296571805b17d2ee3cdd04eb3b9e3
ranhaiqiang <ranhaiqiang@wanglibank.com>
2017-06-19 14:09:23 +0800

    签到摇一摇活动为下午18点开始

--- config/signinsystem.php
+++ config/signinsystem.php
@@ -34,7 +34,7 @@ return [
     'lists' => [
         [
             'start' => 11,
-            'end' => 17,
+            'end' => 18,
             'times' => 60*15, // 活动最长持续时间(秒),到时间后强制结束
             'awards' => [
                 ['alias_name' => 'sign_in_system_0.1', 'size' => 0.1, 'num' => 3000, 'is_rmb' => 1],
@@ -46,7 +46,7 @@ return [
                 ['alias_name' => 'sign_in_system_100', 'size' => 0, 'num' => 1000, 'is_rmb' => 0],
             ]
         ], [
-            'start' => 17,
+            'start' => 18,
             'end' => 21,
             'times' => 60*15, // 活动最长持续时间(秒),到时间后强制结束
             'awards' => [
