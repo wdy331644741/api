35e2864a27c4a4434c97997899c4532212c5828d
ranhaiqiang <ranhaiqiang@wanglibank.com>
2017-06-19 18:16:39 +0800

    七月大转盘活动修改

---
+++ app/Jobs/DazhuanpanBatch.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+use App\Service\Attributes;
+use App\Service\DazhuanpanService;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+
+class DazhuanpanBatch extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+    private $userId;
+    private $config;
+    private $awards;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($userId,$config,$awards)
+    {
+        $this->userId = $userId;
+        $this->config = $config;
+        $this->awards = $awards;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        //
+        Attributes::incrementByDay($this->userId, $this->config['drew_daily_key']);
+        Attributes::increment($this->userId, $this->config['drew_total_key']);
+        //发奖
+        foreach($this->awards as $item){
+            DazhuanpanService::sendAward($this->userId, $item);
+        }
+    }
+}
