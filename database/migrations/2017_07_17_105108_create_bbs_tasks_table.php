0894c0735dbca692b6ac703f7c348c5577a7d061
shihe@wanglibank.com <shihe@wanglibank.com>
2017-07-17 11:32:17 +0800

    修改表结构

--- database/migrations/2017_07_17_105108_create_bbs_tasks_table.php
+++ database/migrations/2017_07_17_105108_create_bbs_tasks_table.php
@@ -16,8 +16,13 @@ class CreateBbsTasksTable extends Migration
             $table->increments('id');
             $table->tinyInteger('task_type')->comment('任务类型，1：每日任务，2：成就任务');
             $table->string('name')->comment('任务名称');
-            $table->integer('number');
-            $table->tinyInteger('trigger_type');
+            $table->integer('number')->comment('触发条件');
+            $table->string('task_mark')->unique()->comment('任务唯一性标志');
+            $table->string('description')->comment('任务描述');//前端展示
+            $table->tinyInteger('trigger_type')->comment('触发节点');//comment //thread //commentZan //threadZan //threadCollections
+            $table->tinyInteger('award_type')->comment('奖励类型');//1 体验金
+            $table->integer('award')->comment('奖励数量');//奖励数量
+            $table->tinyInteger('enable')->default(0)->comment("启用 0 未启用 1 启用");
             $table->string('remark')->nullable()->default(NULL);
             $table->timestamps();
         });
