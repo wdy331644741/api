199bc4327c81957ee3da1c3516a33cfc8b0a681e
zdr <zhaodongran@wanglibank,com>
2017-07-17 17:28:30 +0800

    社区任务

--- app/Http/Controllers/Bbs/TaskController.php
+++ app/Http/Controllers/Bbs/TaskController.php
@@ -7,8 +7,26 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Validator;
+use App\Http\Traits\BasicDatatables;
+use App\Models\Bbs\Tasks;
 
 class TaskController extends Controller
 {
+    use BasicDataTables;
+    protected $model = null;
+    protected $fileds = ['id','task_type', 'name', 'number', 'trigger_type','remark','task_type','description','award_type','award','frequency','created_at'];
+    protected $deleteValidates = [
+        'id'=>'required|exists:bbs_tasks,id',
+    ];
+    protected $addValidates = ['task_type','name','number','trigger_type','task_type','award_type','award','frequency'];
+    protected $updateValidates = [];
 
+    function __construct() {
+        $this->model = new Tasks();
+    }
+
+    public function getIndex(Request $request)
+    {
+        return "ok";
+    }
 }
