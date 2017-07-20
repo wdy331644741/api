4876c4f4b721cdbc55ae464876e86640089d6052
zdr <zhaodongran@wanglibank,com>
2017-07-20 14:35:20 +0800

    社区任务接口

--- app/Http/Controllers/Bbs/TaskController.php
+++ app/Http/Controllers/Bbs/TaskController.php
@@ -9,24 +9,25 @@ use App\Http\Controllers\Controller;
 use Validator;
 use App\Http\Traits\BasicDatatables;
 use App\Models\Bbs\Tasks;
+use Config;
 
 class TaskController extends Controller
 {
     use BasicDataTables;
     protected $model = null;
-    protected $fileds = ['id','task_type', 'name', 'number', 'trigger_type','remark','task_type','description','award_type','award','frequency','created_at'];
+    protected $fileds = ['id', 'name', 'task_type', 'number', 'trigger_type','award_type','award','frequency','created_at'];
     protected $deleteValidates = [
         'id'=>'required|exists:bbs_tasks,id',
     ];
-    protected $addValidates = ['task_type','name','number','trigger_type','task_type','award_type','award','frequency'];
+    protected $addValidates = ['task_type','name','number','trigger_type','task_mark','award_type','award','frequency'];
     protected $updateValidates = [];
 
     function __construct() {
         $this->model = new Tasks();
     }
 
-    public function getIndex(Request $request)
+    public function getTriggerType()
     {
-        return "ok";
+        return $this->outputJson(0,Config::get('bbstask.trigger_type'));
     }
 }
