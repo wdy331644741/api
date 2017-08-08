35c8a503143cb136b360aed690f90780832b0ee1
zhaodongran <zhaodongran@wanglibank.com>
2017-08-08 14:41:29 +0800

    官方帖

--- app/Http/Controllers/Bbs/ThreadController.php
+++ app/Http/Controllers/Bbs/ThreadController.php
@@ -22,7 +22,7 @@ class ThreadController extends Controller
 {
     use BasicDataTables;
     protected $model = null;
-    protected $fileds = ['id','user_id','title','content', 'type_id','video_code','created_at', 'istop', 'isgreat', 'ishot', 'isverify', 'comment_num','zan_num','collection_num'];
+    protected $fileds = ['id','user_id','title','content', 'type_id','video_code','created_at', 'istop', 'isgreat', 'ishot','isofficial','isverify', 'comment_num','zan_num','collection_num'];
     protected $deleteValidates = [
         'id' => 'required|exists:bbs_threads,id'
     ];
