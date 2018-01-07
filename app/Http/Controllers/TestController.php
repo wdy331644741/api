af2edc234c0319e427a61722b361d3822ed744f1
zhaodongran <zhaodongran@wanglibank.com>
2018-01-07 14:43:00 +0800

    导出评论去除登录验证

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -352,40 +352,6 @@ class TestController extends Controller
         $res = Cqssc::where('opentime', '>=',$date )->orderBy('expect', 'asc')->first();
     }
 
-    //导出帖子评论
-    public function getExportGxfcExecl($tid,$date){
-        $start_date = date('Y-m-d 00:00:00',strtotime($date));
-        $end_date = date('Y-m-d 00:00:00',strtotime($date."+ 1 day"));
-        $data = Comment::select('user_id','content','created_at')->where(['tid'=>$tid,'isverify'=>1])
-            ->where('created_at','>=',$start_date)
-            ->where('created_at','<',$end_date)
-            ->orderBy('created_at','desc')
-            ->get()
-            ->toArray();
-        foreach($data as $key => $item){
-            if($key == 0){
-                $cellData[$key] = array('用户ID','评论内容','创建时间');
-            }
-            $cellData[$key+1] = array($item['user_id'],$item['content'],$item['created_at']);
-        }
-        $fileName = $date.'-评论列表';
-        $typeName = "xls";
-        Excel::create($fileName,function($excel) use ($cellData,$fileName){
-            $excel->sheet($fileName, function($sheet) use ($cellData){
-                $sheet->cells("A1:C1",function ($cells){
-                    $cells->setBackground('#C5E1BA');
-                    $cells->setFontWeight('bold');
-                });
-                $sheet->setWidth(array(
-                    'A'=>10,
-                    'B'=>20,
-                    'C'=>20,
-                ));
-                $sheet->rows($cellData);
-            });
-        })->export($typeName);
-    }
-
     public function getUserInfo() {
         $userId = 1231312312;
         $client = new JsonRpcClient(env('INSIDE_HTTP_URL'));
