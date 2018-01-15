a51e62fb9e326c6c57b85d4e242efb72c64dfb9c
zhaodongran <zhaodongran@wanglibank.com>
2018-01-15 17:39:28 +0800

    微信自动回复链接修改

--- app/Http/Controllers/OpenController.php
+++ app/Http/Controllers/OpenController.php
@@ -275,7 +275,7 @@ class OpenController extends Controller
                      */
                 }
                 $content['error'] = 0;
-                $content['content'] = "网利宝已服务超过250万用户，累计为用户创造收益近5亿元。网利宝，网利宝，稳健收益就选网利宝！\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=2&sn=ee5d18c471d2761ddd63d36309345254#wechat_redirect'>点击查看投资军师相关信息</a>\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=3&sn=167284bb8f6721c5461bff4e93a79179#wechat_redirect'>点击查看往期见面会信息</a>\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=4&sn=f79906779226a285edd10a1634f49506#wechat_redirect '>点击查看网利宝最新动态</a>\n\n点击进入<a href='http://wanglibao.udesk.cn/im_client/'>【在线客服】</a>咨询,可以随时向客服MM咨询问题哦,等你~";
+                $content['content'] = "网利宝已服务超过250万用户，累计为用户创造收益近5亿元。网利宝，网利宝，稳健收益就选网利宝！\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=2&sn=ee5d18c471d2761ddd63d36309345254#wechat_redirect'>点击查看投资军师相关信息</a>\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=3&sn=167284bb8f6721c5461bff4e93a79179#wechat_redirect'>点击查看往期见面会信息</a>\n\n<a href='http://mp.weixin.qq.com/mp/homepage?__biz=MzA5NzE4NTIzMQ==&hid=4&sn=f79906779226a285edd10a1634f49506#wechat_redirect'>点击查看网利宝最新动态</a>\n\n点击进入<a href='http://wanglibao.udesk.cn/im_client/'>【在线客服】</a>咨询,可以随时向客服MM咨询问题哦,等你~";
                 break;
             case "unsubscribe":
                 /**
