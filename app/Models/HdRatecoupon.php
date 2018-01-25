e90d66c64eeef2581349520c87cecc70453f7a27
pangxiaofei <pangxiaofei@wanglibank.com>
2018-01-25 15:57:40 +0800

    邀请好友先提交代码

--- app/Models/HdRatecoupon.php
+++ app/Models/HdRatecoupon.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class HdRatecoupon extends Model
 {
     public $table = 'hd_ratecoupon';
-    protected $guarded = ['created_at', 'update_at'];
+    protected $guarded = ['created_at', 'updated_at'];
 }
