62b13219351c1a76830f23ec5295505046069369
shihe@wanglibank.com <shihe@wanglibank.com>
2018-03-28 15:17:41 +0800

    u

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendAwards;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,7 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sendawards')
-                  ->everyMinute();
+        $schedule->call(function () {
+            SendAwards::handle();
+        })->everyMinute();
+
     }
 }
