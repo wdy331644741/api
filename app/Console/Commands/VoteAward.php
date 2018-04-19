741747748fb4784d5494c392280011ac2c0599d7
IzhuangbeiWdy <wdy@izhuanbei.com>
2018-04-19 14:32:28 +0800

    添加自动 发奖任务

---
+++ app/Console/Commands/VoteAward.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\ActivityVote;
+use Illuminate\Support\Facades\Redis;
+use App\Service\ActivityService;
+use App\Models\Activity;
+use App\Service\SendAward;
+
+class VoteAward extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'VoteAward';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Display an inspiring quote';
+
+    private static $victoryAward = [
+        "id" => 0,
+        "name" => "2%加息券",
+        "rate_increases" => 0.02,
+        "rate_increases_type" => 1,
+        "rate_increases_start" => null,
+        "rate_increases_end" => null,
+        "effective_time_type" => 1,
+        "effective_time_day" => 10,
+        "effective_time_start" => null,
+        "effective_time_end" => null,
+        "investment_threshold" => 10000,
+        "project_duration_type" => 2,
+        "project_type" => 0,
+        "product_id" => "",
+        "platform_type" => 0,
+        "limit_desc" => "10000元起投，限6月标",
+        "created_at" => "",
+        "updated_at" => "",
+        "rate_increases_time" => 0,
+        "project_duration_time" => 6,
+        "message" => "恭喜您在'{{sourcename}}'活动中获得'{{awardname}}'奖励。",
+        "mail" => "恭喜您在'{{sourcename}}'活动中获得'{{awardname}}'奖励。",
+        "source_id" => "",
+        "source_name" => "",
+        "trigger" => 4,
+        "user_id" => "",
+    ];
+
+    private static $lostAward = [
+        "id" => 0,
+        "name" => "1.2%加息券",
+        "rate_increases" => 0.012,
+        "rate_increases_type" => 1,
+        "rate_increases_start" => null,
+        "rate_increases_end" => null,
+        "effective_time_type" => 1,
+        "effective_time_day" => 10,
+        "effective_time_start" => null,
+        "effective_time_end" => null,
+        "investment_threshold" => 10000,
+        "project_duration_type" => 2,
+        "project_type" => 0,
+        "product_id" => "",
+        "platform_type" => 0,
+        "limit_desc" => "10000元起投，限6月标",
+        "created_at" => "",
+        "updated_at" => "",
+        "rate_increases_time" => 0,
+        "project_duration_time" => 6,
+        "message" => "恭喜您在'{{sourcename}}'活动中获得'{{awardname}}'奖励。",
+        "mail" => "恭喜您在'{{sourcename}}'活动中获得'{{awardname}}'奖励。",
+        "source_id" => "",
+        "source_name" => "",
+        "trigger" => 4,
+        "user_id" => "",
+    ];
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // $this->comment(PHP_EOL.Inspiring::quote().PHP_EOL);
+        $activityName = 'vote_time';
+        // 活动是否存在
+        if(!ActivityService::isExistByAlias($activityName)) {
+            throw new OmgException(OmgException::ACTIVITY_NOT_EXIST);
+        }
+        $activityTime = ActivityService::GetActivityedInfoByAlias($activityName);
+        // if(date('Y-m-d H:i:s') < $activityTime['end_at']){
+        //     return 0;
+        // }
+        $planA = Redis::zCard('planA_list');
+        $planB = Redis::zCard('planB_list');
+
+        if(!$planA){
+            $planA = ActivityVote::where(['vote'=> 'planA'])->count();
+        }
+        if(!$planB){
+            $planB = ActivityVote::where(['vote'=> 'planB'])->count();
+        }
+        $victoryOptioin = ($planA>$planB)?'planA':'planB';
+        $lostOptioin = ($planA>$planB)?'planB':'planA';
+        $victorylist = Redis::zRange($victoryOptioin."_list" , 0 ,-1);
+        $lostlist = Redis::zRange($lostOptioin."_list" , 0 ,-1);
+        foreach ($victorylist as $v) {
+            $this->sendAward($v,$activityTime,'victory');
+        }
+        foreach ($lostlist as $v) {
+            $this->sendAward($v,$activityTime,'lost');
+        }
+        // dd($list);
+        // dd($activityTime['end_at']);
+    }
+
+    private function sendAward($userId ,$activity,$type){
+        if (!SendAward::frequency($userId, $activity)) {//不通过
+            //添加到活动参与表 1频次验证不通过2规则不通过3发奖成功
+            return SendAward::addJoins($userId, $activity, 1, json_encode(array('err_msg' => 'pass frequency')));
+        }
+        //*****活动参与人数加1*****
+        Activity::where('id',$activity['id'])->increment('join_num');
+        //直抵红包相关参数
+        if($type == 'victory'){
+            self::$victoryAward['source_id'] = $activity['id'];
+            self::$victoryAward['source_name'] = $activity['name'];
+            self::$victoryAward['user_id'] = $userId;
+            $result = SendAward::increases(self::$victoryAward);
+        }else{
+            self::$lostAward['source_id'] = $activity['id'];
+            self::$lostAward['source_name'] = $activity['name'];
+            self::$lostAward['user_id'] = $userId;
+            $result = SendAward::increases(self::$lostAward);
+        }
+
+        //添加活动参与记录
+        if($result['status']){
+            SendAward::addJoins($userId,$activity,3);
+            
+        }
+    }
+
+
+}
