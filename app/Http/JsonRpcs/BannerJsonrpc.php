7521b4326ce1bfb02d6e10976a619c90607c7c12
pangxiaofei <pangxiaofei@wanglibank.com>
2018-04-24 10:23:28 +0800

    会员卡渠道更新

--- app/Http/JsonRpcs/BannerJsonrpc.php
+++ app/Http/JsonRpcs/BannerJsonrpc.php
@@ -487,7 +487,7 @@ class BannerJsonRpc extends JsonRpc {
             return $data;
         }
         $channel = [
-            '360jj','360pcss','360ydss','APPStore','APPStorePlus','baidu','baidujj','baidupz','chuizi','fwh','huawei','lenovo','m360','m360jj','mbaidujj','mbaidupz','meizu','oppo','oppofeed','qq','qqplus','qqplus1','qqplus2','samsung','sgqqdh','sogou','sougou1','vivo','wanglibao1','xiaomi','xiaomiplus','ali','qqcpd','gdt','gdt1','sglccpt','360pz','m360pz','fhcpc','jrttcpc','sgpcss','sgydss','sgpz','msgpz','qqcpd1','ggkdg','wanglibao2','xlpx',
+            '360jj','360pcss','360ydss','APPStore','APPStorePlus','baidu','baidujj','baidupz','chuizi','fwh','huawei','lenovo','m360','m360jj','mbaidujj','mbaidupz','meizu','oppo','oppofeed','qq','qqplus','qqplus1','qqplus2','samsung','sgqqdh','sogou','sougou1','vivo','wanglibao1','xiaomi','xiaomiplus','ali','qqcpd','gdt','gdt1','sglccpt','360pz','m360pz','fhcpc','jrttcpc','sgpcss','sgydss','sgpz','msgpz','qqcpd1','ggkdg','wanglibao2','xlpx','fh1','fh2','fh3','fh4','fh5'
         ];
         if (in_array($thisChannel, $channel)) {
             $where = ['position' => $position, 'can_use' => 0, 'name' => "特定渠道显示，快乐大本营送芒果月卡"];
