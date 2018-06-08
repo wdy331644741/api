f2c87ede5cfd3f00052aa737e45ae482fe90a2a7
ranhaiqiang <ranhaiqiang@wanglibank.com>
2018-06-08 10:27:45 +0800

    世界杯活动配置修改

--- app/Http/Controllers/WorldCupConfigController.php
+++ app/Http/Controllers/WorldCupConfigController.php
@@ -13,13 +13,13 @@ class WorldCupConfigController extends Controller
 {
     use BasicDataTables;
     protected $model = null;
-    protected $fileds = ['id', 'team', 'number', 'created_at', 'updated_at'];
+    protected $fileds = ['id', 'team', 'number'];
     protected $deleteValidates = [
-        'id' => 'required|exists:admins,id',
+        'id' => 'required|exists:hd_world_cup_config,id',
     ];
     protected $addValidates = [];
     protected $updateValidates = [
-        'id' => 'required|exists:admins,id',
+        'id' => 'required|exists:hd_world_cup_config,id',
     ];
 
     function __construct() {
--- app/Models/HdWorldCupConfig.php
+++ app/Models/HdWorldCupConfig.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
 class HdWorldCupConfig extends Model
 {
+    public $table = 'hd_world_cup_config';
+    protected $guarded = ['created_at', 'update_at'];
     protected $hidden = ['deleted_at'];
 
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
-    public $table = 'hd_world_cup_config';
 }
