158271c7327005b2829c92a9279d4d0d24f0b373
ranhaiqiang <ranhaiqiang@wanglibank.com>
2018-11-02 13:16:05 +0800

    曲棍球竞猜数据库修改

--- database/migrations/2018_10_23_095923_create_table_hd_hockey_guess_config.php
+++ database/migrations/2018_10_23_095923_create_table_hd_hockey_guess_config.php
@@ -17,13 +17,13 @@ class CreateTableHdHockeyGuessConfig extends Migration
             $table->string('match_date',10)->default('')->comment('比赛日期');
             $table->string('first',5)->default('')->comment('第一场对阵1-2 意思：（中国-荷兰）');
             $table->string('first_score',5)->default('')->comment('第一场比分');
-            $table->tinyInteger('first_result')->default(0)->comment('第一场结果1主胜，2平，3客胜');
+            $table->tinyInteger('first_result')->default(0)->comment('第一场结果1主胜，2平，3主负');
             $table->string('second',5)->default('')->comment('第二场对阵1-2 意思：（中国-荷兰）');
             $table->string('second_score',5)->default('')->comment('第二场比分');
-            $table->tinyInteger('second_result')->default(0)->comment('第二场结果1主胜，2平，3客胜');
+            $table->tinyInteger('second_result')->default(0)->comment('第二场结果1主胜，2平，3主负');
             $table->string('third',5)->default('')->comment('第三场对阵1-2 意思：（中国-荷兰）');
             $table->string('third_score',5)->default('')->comment('第三场比分');
-            $table->tinyInteger('third_result')->default(0)->comment('第三场结果1主胜，2平，3客胜');
+            $table->tinyInteger('third_result')->default(0)->comment('第三场结果1主胜，2平，3主负');
             $table->tinyInteger('msg_status')->default(0)->comment('站内信发送状态0未发送，1已发送');
             $table->tinyInteger('champion_status')->default(0)->comment('是否是冠军场0否1是');
             $table->string("draw_info")->default('')->comment('中奖信息1_first_1,1_second_2,1_third_3方便查询find_name');
