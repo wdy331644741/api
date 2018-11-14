577133b32f3f3e44b14ebbbacf6920f397e22bcf
zhaodongran <zhaodongran@wanglibank.com>
2018-11-14 14:22:21 +0800

    积分商城后台修改

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -61,6 +61,7 @@ Route::group(['middleware' => 'admin'], function(){
     Route::controller('mark', 'MarkController');
     // 积分商城
     Route::controller('integral', 'IntegralMallController');
+    Route::controller('prize', 'InPrizeController');
     // 1元夺宝
     Route::controller('one', 'OneYuanController');
     // 红包分享
