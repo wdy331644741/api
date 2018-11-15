0fe06f34266b5ebd8c2afe81d99d8986abd178cd
ranhaiqiang <ranhaiqiang@wanglibank.com>
2018-11-15 14:13:43 +0800

    合并冲突

--- app/Http/JsonRpcs/BannerJsonrpc.php
+++ app/Http/JsonRpcs/BannerJsonrpc.php
@@ -490,6 +490,7 @@ class BannerJsonRpc extends JsonRpc {
                 $query->whereNull('end')->orWhereRaw('end > now()');
             })
             ->orderByRaw('sort DESC')->first();
+
         if(isset($data->id)){
             return array(
                 'code' => 0,
