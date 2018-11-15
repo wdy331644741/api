059ff1347e24d818815ff28df202f089ecfa9cd8
IzhuangbeiWdy <wdy@izhuanbei.com>
2018-11-15 16:45:51 +0800

    邀请投资送次数

--- app/Service/SendAward.php
+++ app/Service/SendAward.php
@@ -203,6 +203,7 @@ class SendAward
                     && isset($triggerData['user_id']) && !empty($triggerData['user_id']) 
                     && $triggerData['tag'] == 'investment' 
                     ){
+                    $key = 'open_gift';//活动标示
                     //新用户 首投满2000元获得一次拆礼物机会、4000元2次
                     //当日注册并首投 额外再加一次
 
@@ -217,7 +218,7 @@ class SendAward
                         }
 
                         if($triggerData['from_user_id']){
-                            $fromUserIsNew = Func::isNewUser($triggerData['from_user_id']);var_dump($fromUserIsNew);
+                            $fromUserIsNew = Func::isNewUser($triggerData['from_user_id']);
                             if($fromUserIsNew && $triggerData['Investment_amount'] >+ 500){
                                 $investmentNum += 1;
                                 $fromUserNum += 1;
@@ -225,8 +226,8 @@ class SendAward
                         }
                     }
 
-                    var_dump($investmentNum);
-                    var_dump($fromUserNum);
+                    Attributes::increment($triggerData['user_id'] ,$key ,$investmentNum);
+                    Attributes::increment($triggerData['from_user_id'] ,$key ,$fromUserNum);
 
                     
 
