f41a5be1737e7c161576ea3f9cf8aa7a2b1bdd5c
pangxiaofei <pangxiaofei@wanglibank.com>
2018-11-22 15:27:48 +0800

    分享url

--- app/Service/SendMessage.php
+++ app/Service/SendMessage.php
@@ -67,10 +67,12 @@ class SendMessage
     }
 
     public static function sendPush($userID,$nodeName,$arr=[]){
-        $params = array();
-        $params['user_id'] = $userID;
-        $params['node_name'] = $nodeName;
-        $params['tplParam'] = $arr;
+        $params = new \stdClass();
+        $params->user_id = $userID;
+        $params->node_name = $nodeName;
+//        $params['user_id'] = $userID;
+//        $params['node_name'] = $nodeName;
+//        $params['tplParam'] = $arr;
         $url = Config::get('cms.message_http_url');
         $client = new JsonRpcClient($url);
         $res = $client->sendJpush($params);
