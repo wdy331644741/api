2f1892d3e06536495f5fd95a95a179592d41279d
zhaodongran <zhaodongran@wanglibank.com>
2019-01-10 11:01:21 +0800

    新年红包用户属性表

---
+++ app/Models/Hd19AmountShareAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hd19AmountShareAttribute extends Model
+{
+    public $table = 'hd_19amountshare_attributes';
+}
+
---
+++ database/migrations/2019_01_10_103557_create_hd_19amountshare_attributes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHd19amountshareAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hd_19amountshare_attributes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id')->index();
+            $table->string('key')->index();
+            $table->integer('datenum')->index();
+            $table->decimal('amount')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hd_19amountshare_attributes');
+    }
+}
