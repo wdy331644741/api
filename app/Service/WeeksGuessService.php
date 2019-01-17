381429930e07846f63ace76ff256aa7370babda3
pangxiaofei <pangxiaofei@wanglibank.com>
2019-01-17 17:00:32 +0800

    短信模板修改和次数清0

--- app/Service/WeeksGuessService.php
+++ app/Service/WeeksGuessService.php
@@ -6,13 +6,15 @@ use App\Service\GlobalAttributes;
 use App\Models\GlobalAttribute;
 use App\Models\HdWeeksGuess;
 use App\Models\HdWeeksGuessLog;
+use App\Models\HdWeeksGuessConfig;
+use App\Models\UserAttribute;
 use Config, Cache,DB;
 
 
 class WeeksGuessService
 {
-    public static $messageTpl = "亲爱的用户，周末竞猜专享活动已开奖，恭喜您获得现金{{money}}元，奖励将于比赛休息日发放至您的网利宝账户，请届时注意查收。";
-    public static $numTpl = "亲爱的用户，周末竞猜专享活动，恭喜您获得竞猜次数{{count}}次，请届时注意查收。";
+    public static $messageTpl = "亲爱的用户，周末专享竞猜比赛活动{{race_time}}{{home_team}}队VS{{guest_team}}已开奖，恭喜您获得现金{{money}}元，奖励将于开奖后三个工作日发放至您的网利宝账户，请注意查收。客服电话：400-858-8066";
+    public static $numTpl = "亲爱的用户，恭喜您在周末专享竞猜比赛活动中获得{{count}}次竞猜机会，参与竞猜比赛结果，竞猜正确即可参与瓜分20000元现金";
     public static function sendAward($period, $totalMoney, $type)
     {
         //总次数
@@ -25,9 +27,15 @@ class WeeksGuessService
         if (!$data) {
             return false;
         }
+        $weekConfig = HdWeeksGuessConfig::find($period);
         foreach ($data as $v) {
             $money = bcdiv(bcmul($totalMoney, $v['total'], 2), $totalCount, 2);
-            $tplParam = ['money'=>$money];
+            $tplParam = [
+                'money'=>$money,
+                'home_team'=>$weekConfig->home_team,
+                'guest_team'=>$weekConfig->guest_team,
+                'race_time'=>$weekConfig->race_time,
+            ];
             SendMessage::Mail($v['user_id'], self::$messageTpl, $tplParam);
             SendMessage::Message($v['user_id'], self::$messageTpl, $tplParam);
             $insert = HdWeeksGuessLog::create([
@@ -37,6 +45,8 @@ class WeeksGuessService
             ]);
             HdWeeksGuess::where(['period'=>$period, 'type'=>$type, 'user_id'=>$v['user_id']])->update(['status'=>1]);
         }
+        //次数清0
+        UserAttribute::where(['key'=>'weeksguess_drew_user'])->update(['number'=>0]);
         return true;
     }
 
