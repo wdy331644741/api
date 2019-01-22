5d3c8f161041260283c9c9971f331474f1885667
zhaodongran <zhaodongran@wanglibank.com>
2019-01-22 14:49:44 +0800

    红包中心接口，再途奖励变更

--- app/Http/JsonRpcs/AmountShare19Jsonrpc.php
+++ app/Http/JsonRpcs/AmountShare19Jsonrpc.php
@@ -129,12 +129,20 @@ class AmountShare19JsonRpc extends JsonRpc
         $type = $params->type;
         $page = isset($params->page) ? $params->page : 1;
         $isHadAll = Hd19AmountShare::where(['share_user_id'=>$userId])->where('receive_status','>=',2)->sum('amount');
-        $isReceiveAll = Hd19AmountShare::where(['user_id'=>$userId,'receive_status'=>1])->sum('amount');
-        $isOnwayAll = Hd19AmountShare::where(['share_user_id'=>$userId,'receive_status'=>1])->sum('amount');
+        $isReceiveAll = Hd19AmountShare::where(['user_id'=>$userId])->where('receive_status','>=',2)->sum('amount');
+        $isOnwayAll = Hd19AmountShare::where(['receive_status'=>1])
+            ->Where(function($query)use($userId){
+            $query->where('share_user_id',$userId)
+                ->orWhere('user_id',$userId);
+        })->sum('amount');
         $data['isReceiveAll'] = $isReceiveAll + $isHadAll;
         $data['isNotReceive'] = $isOnwayAll;
         $data['isHadNum'] = Hd19AmountShare::selectRaw('id,phone,amount,created_at')->where(['share_user_id'=>$userId])->where('receive_status','>=',2)->count();
-        $data['isOnwayNum'] = Hd19AmountShare::selectRaw('id,phone,amount,created_at')->where(['share_user_id'=>$userId,'receive_status'=>1])->count();
+        $data['isOnwayNum'] = Hd19AmountShare::selectRaw('id,phone,amount,created_at')->where(['receive_status'=>1])
+            ->Where(function($query)use($userId){
+            $query->where('share_user_id',$userId)
+                ->orWhere('user_id',$userId);
+        })->count();
         $data['isReceiveNum'] = Hd19AmountShare::selectRaw('id,share_phone as phone,amount,created_at')->where(['user_id'=>$userId])->where('receive_status','>=',2)->count();
         $userinfo = Func::getUserBasicInfo($userId,true);
         $data['display_name'] = $userinfo['display_name'];
@@ -157,7 +165,11 @@ class AmountShare19JsonRpc extends JsonRpc
                 $data['data'] = $isHad;
                 break;
             case 'isOnway':
-                $isOnway = Hd19AmountShare::selectRaw('id,phone,amount,created_at')->where(['share_user_id'=>$userId,'receive_status'=>1])->orderBy('id','desc')->paginate(10)->toArray();
+                $isOnway = Hd19AmountShare::selectRaw('id,phone,amount,created_at')->where(['receive_status'=>1])
+                    ->Where(function($query)use($userId){
+                    $query->where('share_user_id',$userId)
+                        ->orWhere('user_id',$userId);
+                })->orderBy('id','desc')->paginate(10)->toArray();
                 if(empty($isOnway['data'])){
                     $data['data'] = $isOnway;
                 }
