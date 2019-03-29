fe6c1d1df9efdc3e9b8b12af39ca0e67ab416fff
pangxiaofei <pangxiaofei@wanglibank.com>
2019-03-29 10:58:54 +0800

    踏青

--- config/spring.php
+++ config/spring.php
@@ -5,14 +5,14 @@ return [
     'spring_drew_user' => 'spring_drew_user',//用户剩余基金
     'spring_drew_total' => 'spring_drew_total',//总共领取基金
     'awards' => [
-        ['name'=>'5元京东卡', 'alias_name'=>'jingdongka', 'fund'=> 1500],
-        ['name'=>'爱奇艺会员月卡', 'alias_name'=>'iqiyi', 'fund'=> 5800],
-        ['name'=>'小米移动电源2C 2000毫安', 'alias_name'=>'xiaomi', 'fund'=> 27000],
-        ['name'=>'美固车载冰箱20L', 'alias_name'=>'bingxiang', 'fund'=> 125000],
-        ['name'=>'JBL Pulse 3蓝牙音箱', 'alias_name'=>'yinxiang', 'fund'=> 440000],
-        ['name'=>'尼康D5600 18-140单反相机', 'alias_name'=>'xiangji', 'fund'=> 900000],
-        ['name'=>'大疆无人机“御”MavicPro 铂金 版套装', 'alias_name'=>'wurenji', 'fund'=> 1250000],
-        ['name'=>'iPhone XS 256G', 'alias_name'=>'iphonex', 'fund'=> 1500000],
-        ['name'=>'MacBook Pro 13英寸', 'alias_name'=>'macbook', 'fund'=> 1700000],
+        ['name'=>'5元京东卡', 'alias_name'=>'jingdongka', 'fund'=> 1450],
+        ['name'=>'爱奇艺会员月卡', 'alias_name'=>'iqiyi', 'fund'=> 5500],
+        ['name'=>'小米移动电源2C', 'alias_name'=>'xiaomi', 'fund'=> 33000],
+        ['name'=>'美固车载冰箱20L', 'alias_name'=>'bingxiang', 'fund'=> 100000],
+        ['name'=>'JBL Pulse 3蓝牙音箱', 'alias_name'=>'yinxiang', 'fund'=> 400000],
+        ['name'=>'尼康D5600单反相机', 'alias_name'=>'xiangji', 'fund'=> 1050000],
+        ['name'=>'大疆无人机', 'alias_name'=>'wurenji', 'fund'=> 1350000],
+        ['name'=>'iPhone XS 256G', 'alias_name'=>'iphonex', 'fund'=> 1400000],
+        ['name'=>'MacBook Pro 13英寸', 'alias_name'=>'macbook', 'fund'=> 1500000],
     ]
 ];
