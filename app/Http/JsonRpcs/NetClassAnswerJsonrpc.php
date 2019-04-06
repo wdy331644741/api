c4f92b0aeebfce55f6fc2e0a33f74e210e003c53
IzhuangbeiWdy <wdy@izhuanbei.com>
2019-04-06 18:57:13 +0800

    网带课堂 答题奖励

--- app/Http/JsonRpcs/NetClassAnswerJsonrpc.php
+++ app/Http/JsonRpcs/NetClassAnswerJsonrpc.php
@@ -33,22 +33,37 @@ class NetClassAnswerJsonRpc extends JsonRpc {
             $award_info = Activity::where('alias_name', $value['alias_name'])->with('awards')->first()->awards;
             $award_data = SendAward::_getAwardInfo($award_info[0]['award_type'],$award_info[0]['award_id']);
             $plan_key = str_replace(self::$_plan,'',$value['alias_name']);
+
+
+            $answer_status = isset($status_data[$plan_key])?$status_data[$plan_key]:0;
+            if(empty($value['des'])){
+                throw new OmgException(OmgException::PARSE_ERROR);
+            }
+            $plan_status = pow(2,$value['des']) - 1 - $answer_status;
+
+            if ($key >0) {
+                //上一plan 是否完成
+                $befor_str = pow(2,$activits[$key-1]['des']) - 1 - (isset($status_data[$plan_key-1])?$status_data[$plan_key-1]:0);
+                $str = $befor_str?'待解锁':'立即学习';
+            }else{
+                $str = $plan_status?'立即学习':'已完成';
+            }
+
             $res[$plan_key] = [
+                'answer_status' => $answer_status,
                 'award' => $award_data['name'],
+                'plan_status' => $str
             ];
+
         }
-        return $res;
+
         //plan状态：立即学习，待解锁，已完成，待上线
         return [
             'code'    => 0,
             'message' => 'success',
             'data'    => [
                 'is_login' => $userId?true:false,
-                'plan_status' => [
-                    1=>0,
-                    2=>-1,
-                    3=>1,
-                ]
+                'plan_status' => $res
             ]
         ];
     }
@@ -75,8 +90,8 @@ class NetClassAnswerJsonRpc extends JsonRpc {
 
         //答案
         $validator = Validator::make([
-            'plan' => $params->plan,
-            'answer' => $params->answer,
+            'plan' => $plan,
+            'answer' => $answer,
         ], [
             'plan' => 'required|integer',
             'answer' => 'required|digits_between:1,7',
@@ -84,9 +99,18 @@ class NetClassAnswerJsonRpc extends JsonRpc {
         if($validator->fails()){
             throw new OmgException(OmgException::PARAMS_ERROR);
         }
-        $plan_status = [$params->plan=>$params->answer];
+        //[$params->plan=>$params->answer];
 
         $user_answered = Attributes::getJsonText($userId,self::$_plan);
+        //不能跳着答题
+        if($plan>1){
+            $befor_act = $plan-1;
+            $temp = ActivityService::GetActivityInfoByAlias(self::$_plan.$befor_act);
+
+            if(!isset($user_answered[$befor_act]) || (pow(2,$temp['des']) - 1 - $user_answered[$befor_act]) >0 ){
+                return '请完成上一攻略';
+            }
+        }
 
         if(!empty($user_answered) && array_key_exists($plan, $user_answered)){
             $user_answered[$plan] |= $answer;//与 位运算（算出最终状态）
@@ -97,9 +121,19 @@ class NetClassAnswerJsonRpc extends JsonRpc {
 
         Attributes::setItem($userId,self::$_plan,0,'',json_encode($user_answered));
         $this->isTooOften($userId, 500 ,0);//删除redis lock
+
+        //是否全部答对、主动发奖
+        $act = ActivityService::GetActivityInfoByAlias(self::$_plan.$plan);
+
+        //放到任务调度里面去
+        if( (pow(2,$act['des']) - 1 - $answer) ==0){
+            SendAward::ActiveSendAward($userId,self::$_plan.$plan);
+        }
+        
         return true;
 
     }
+
 
     //haomiao  锁毫秒
     //status 1加锁、0解锁
