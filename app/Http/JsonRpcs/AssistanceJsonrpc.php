9837255573d484068d41edccbd9c944959d88e10
ranhaiqiang <ranhaiqiang@wanglibank.com>
2019-04-12 14:14:03 +0800

    助力修改

--- app/Http/JsonRpcs/AssistanceJsonrpc.php
+++ app/Http/JsonRpcs/AssistanceJsonrpc.php
@@ -319,6 +319,13 @@ class AssistanceJsonRpc extends JsonRpc
                     $i++;
                 }
             }
+            if(count($res) > 0){
+                $result = [];
+                foreach($res as $val){
+                    $result[] = $val;
+                }
+                return $result;
+            }
         }
         return $res;
     }
