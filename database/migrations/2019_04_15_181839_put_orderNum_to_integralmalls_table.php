f35f34f117f7d9ccb6d609d4a870a9ce29b080a7
zhaodongran <zhaodongran@wanglibank.com>
2019-04-15 18:22:38 +0800

    积分商城增加兑吧订单号字段长度

---
+++ database/migrations/2019_04_15_181839_put_orderNum_to_integralmalls_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PutOrderNumToIntegralmallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('integralmalls', function (Blueprint $table) {
+            $table->string('orderNum',50)->comment('兑吧订单号')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('integralmalls', function (Blueprint $table) {
+            $table->string('orderNum',20)->comment('兑吧订单号')->index();
+        });
+    }
+}
---
+++ database/migrations/2019_04_15_181937_put_orderNum_to_integralmall_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PutOrderNumToIntegralmallLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('integralmall_logs', function (Blueprint $table) {
+            $table->string('orderNum',50)->comment('兑吧订单号')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('integralmall_logs', function (Blueprint $table) {
+            $table->string('orderNum',20)->comment('兑吧订单号')->index();
+        });
+    }
+}
